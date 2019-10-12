39006b780d9b46d135cbd4f4871ac7196bfff7b2
ismail <ismailsoylemez272@gmail.com>
2019-10-12 22:04:31 +0300

    DataAnnotasions

---
+++ MyOnlineNotesEntitie/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace MyOnlineNotesEntities
+{
+    [Table("Categories")]
+   public  class Category : MyEntityBase
+    {
+        [Required,StringLength(50)]
+        public string Title { get; set; }
+
+        [StringLength(200)]
+        public string Description { get; set; }
+
+
+        public virtual List<Note> Notes { get; set; }
+    }
+}
---
+++ MyOnlineNotesEntitie/Liked.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace MyOnlineNotesEntities
+{
+    [Table("Likes")]
+   public class Liked
+    {
+        [Key,DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public virtual Note Note { get; set; }
+        public virtual OnlineNoteUser LikedUser { get; set; }
+    }
+}

a6da1dffd38711fecb6ffd5a3d7a73ff38c6400b
ismail <ismailsoylemez272@gmail.com>
2019-11-10 02:05:07 +0300

    Modal Popup Yorumlar PartialVew

---
+++ MyOnlineNotesWebApp/Controllers/CommentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyOnlineNotesWebApp.Controllers
+{
+    public class CommentController : Controller
+    {
+        // GET: Comment
+        public ActionResult ShowNoteComments()
+        {
+            return PartialView("_PartialComments");
+        } 
+    }
+}
